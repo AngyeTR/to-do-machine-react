0003f0f0d899b3986b4ebb1eb8f1ee99536bc4b5
Angye-TR <angye95@utp.edu.co>
2022-08-09 18:00:49 -0500

    Updates

---
+++ precache-manifest.e2d70748b8202b91cd21fecd531b1dba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "587852117645e693ac66",
+    "url": "/to-do-machine-react/static/css/main.a57bb1c9.chunk.css"
+  },
+  {
+    "revision": "587852117645e693ac66",
+    "url": "/to-do-machine-react/static/js/main.58785211.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/to-do-machine-react/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "72fb2221c95d8f7bd072",
+    "url": "/to-do-machine-react/static/js/runtime~main.72fb2221.js"
+  },
+  {
+    "revision": "229d3ed6e24800d3150df82f9a0983f3",
+    "url": "/to-do-machine-react/index.html"
+  }
+];
